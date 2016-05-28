ecc2cea92d4769dd8e1c1dc81464ab6b7321686f
Devian <forza9882@gmail.com>
2016-05-28 12:39:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <link type="text/css" rel="stylesheet" href="myStyles.css">
+    </head>
+    <body>
+       
+        <div id="infoContainer">
+            <div id="myName">
+                <h1><u>Devian Kapp</u></h1>
+
+                <h4><u>Web Developer</u></h4>
+
+                <h5><u>kapp@devian.co.za</u></h5>
+            </div>
+
+            <div id="myImage">
+                <img src="images/DK4.jpg" alt="jpeg image">
+            </div>
+
+            <h4><u>ABOUT ME</u></h4>
+                <p>
+                    My name is Devian a Cape Town(ZA) based web developer. I passionately use code to craft
+                    creative and user friendly web sites. My current arsenal consist of HTML, CSS and web development
+                    with PHP and MySQL with a dab of Javascript. Contact me at your earliest convenience.I would love to hear about what you wish to
+                    achieve and to help you bring your ideas to life.
+                </p>
+
+                <h4><u>Random Facts about myself(..not in any particular order)</u></h4>
+                
+                    <ul>
+                        <li>Love listening to music</li>
+                        <li>Watching movies</li>
+                        <li>Hardcore gamer</li>
+                        <li>Learn other programming languages</li>
+                        <li>Quest for world domination...one keystroke at a time</li>
+                    </ul>
+               
+
+                <h4><u>Programming Languages</u></h4>
+            <ul>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>PHP and MySQL</li>
+            </ul>
+
+                <h4><u>Education</u></h4>
+            <ul>
+                <li>MasterGradeIT</li>
+            </ul>
+
+            <h4>Contact Me</h4>
+
+            <form>
+                Your name:<br>
+                <input type="text" name="firstname">
+                <br><br>
+                Your contact number:<br>
+                <input type="text" name="phone">
+                <br><br>
+                Your email:<br>
+                <input type="text" name="email">
+                <br><br>
+                Message:<br>
+                <textarea name="message" rows="10" cols="30"></textarea>
+                <br>
+                <input type="submit" value="Send">
+                </form>
+        </div>
+                <div id="footer">
+                    <p>&copy;2016 Devian Kapp</p>
+                </div>
+    </body>
+</html>
